1030fa86e06b8d165926d05678f4d9a10c64b756
jabandersnatch <ja.mendez@uniandes.edu.co>
2022-11-16 20:29:42 -0500

    Maybe we aren't lost just yet

--- casoproofRutasDrones.py
+++ casoproofRutasDrones.py
@@ -4,6 +4,7 @@ Created on Wen Nov 02 09:52:16 2022
 @author: Juan Andrés Méndez G. Erich G
 """
 import pandas as pd
+from pyomo.core.base import label
 from pyomo.environ import *
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,15 +21,15 @@ init_drone_pos = {}
 initial_position = 0
 
 '''
-Create a data dictionary with 4 nodes, 
-one node is the warehouse the other two are delivery_point 
+Create a data dictionary with 7 nodes, 
+2 are a warehouse the other 5 are delivery points
 the location is set in the USA
 the demand is set in the same order as the nodes
 '''
-data = {'node_type': ['warehouse', 'delivery_point', 'delivery_point', 'delivery_point'],
-        'latitude': [42.7173, 40.7128, 41.8781, 39.9526],
-        'longitude': [-73.9897, -74.0060, -87.6298, -75.1652],
-        'demand': [0, 1, 1, 3]}
+data = {'node_type': ['warehouse', 'delivery_point', 'delivery_point', 'delivery_point', 'delivery_point', 'warehouse', 'delivery_point'],
+        'latitude': [42.7173, 40.7128, 41.8781, 39.9526, 38.9072, 39.7545, 40.7128],
+        'longitude': [-73.9897, -74.0060, -80.6298, -75.1652, -77.0369, -77.5021, -76.1232],
+        'demand': [0, 1, 1, 3, 2, 0, 1]}
 
 proof_case = pd.DataFrame(data)
 
@@ -50,7 +51,7 @@ mean_distance_proof_case = np.round(np.mean(list(distances_proof_case.values()))
 
 mean_battery_range = mean_distance_proof_case * 4
 
-n_drones = 1
+n_drones = 2
 
 battery_range_case_2 = np.round(np.random.uniform(0.5 * mean_battery_range, 1.5 * mean_battery_range, n_drones))
 
@@ -68,7 +69,7 @@ for index, row in proof_case.iterrows():
 
 mean_demand_proof_case = np.round(mean_demand_proof_case / len(proof_case[proof_case['node_type']== 'delivery_point']), 0)
 
-mean_capacity_dron = mean_demand_proof_case * 4
+mean_capacity_dron = mean_demand_proof_case * 2
 
 capacity_proof_case = np.round(np.random.uniform(0.5 * mean_capacity_dron, 1.5 * mean_capacity_dron, n_drones))
 
@@ -142,45 +143,60 @@ Model.droneOut = Constraint(drone_set, rule=droneOut)
 fullfillDemand: The drone must fullfill the demand
 '''
 def fullfillDemand(Model, d):
-    return capacity_proof_case[d] >= sum(Model.x[i, j, d] * Model.y[j,d] * demand_proof_case[i][1] for i in nodes_index for j in nodes_index)
+    return sum(Model.x[i,j,d]*Model.y[j,d]*demand_proof_case[j][1] for j in nodes_index for j in nodes_index) <= capacity_proof_case[d]
 Model.fullfillDemand = Constraint(drone_set, rule=fullfillDemand)
 
 '''
-The y variable must be between 0 and 1
+All the delivery points must be visited
 '''
-def toone(model,j,d):
-    return model.y[j,d]<=1
-Model.toone = Constraint(nodes_index, drone_set, rule=toone)
+def allDeliveryPointsMustBeVisited(Model, i):
+    if i in delivery_point_index_proof_case:
+        return sum(Model.x[i,j,d] for j in nodes_index for d in drone_set)==1
+    else:
+        return Constraint.Skip
 
-# Solve the model with ipopt
+Model.allDeliveryPointsMustBeVisited = Constraint(nodes_index, rule=allDeliveryPointsMustBeVisited)
+
+'''
+All the delivery points must be exited
+'''
+def allDeliveryPointsMustBeExited(Model, j):
+    if j in delivery_point_index_proof_case:
+        return sum(Model.x[i,j,d] for i in nodes_index for d in drone_set)==1
+    else:
+        return Constraint.Skip
+    
+Model.allDeliveryPointsMustBeExited = Constraint(nodes_index, rule=allDeliveryPointsMustBeExited)
+
+# Solve the model with quadratic constraints
+
 SolverFactory('ipopt').solve(Model)
+
 
 # Print the results
 Model.display()
 
 import matplotlib.pyplot as plt
 
-# Plot the routes of the drones
+# Plot the routes of the drones in the proof case use a different color for each drone
 
-for d in range(n_drones):
-    for i in proof_case.index:
-        for j in proof_case.index:
-            if Model.x[i, j, d]() == 1:
-                plt.plot([proof_case['latitude'][i], proof_case['latitude'][j]], [proof_case['longitude'][i], proof_case['longitude'][j]], 'k-')
+for d in drone_set:
+    for i in nodes_index:
+        for j in nodes_index:
+            if np.round(Model.x[i,j,d](),0) > 0:
+                plt.plot([proof_case['latitude'][i], proof_case['latitude'][j]], [proof_case['longitude'][i], proof_case['longitude'][j]], color='C'+str(d))
 
 # Plot the nodes of the graph with a different color for the warehouses and the delivery points
 
-plt.plot(proof_case['latitude'][warehouse_index_proof_case], proof_case['longitude'][warehouse_index_proof_case], 'ro')
-plt.plot(proof_case['latitude'][delivery_point_index_proof_case], proof_case['longitude'][delivery_point_index_proof_case], 'bo')
+plt.plot(proof_case['latitude'][warehouse_index_proof_case], proof_case['longitude'][warehouse_index_proof_case], 'ro', label = 'warehouse')
+plt.plot(proof_case['latitude'][delivery_point_index_proof_case], proof_case['longitude'][delivery_point_index_proof_case], 'bo', label = 'delivery_point')
 
-# Create the legend
-plt.legend(['routes','Warehouse', 'Delivery Point'])
 
 # Create the axis
 
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 
-# plt.title('Amazon Delivery Drones Case 2')
+plt.title('Amazon Delivery Drones Proof of Concept')
 
 plt.show()
